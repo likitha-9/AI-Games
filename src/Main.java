d61800f8fe08ef1da159ed344b58358b9073bb27
likitha9 <sivalikitha.valluru9@gmail.com>
2020-05-16 19:51:53 -0400

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String args[]) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
