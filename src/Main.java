5ef8e63a385b48a4049736f08a18a05a28672400
likitha9 <sivalikitha.valluru9@gmail.com>
2020-05-16 20:10:39 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	public static void main(String args[]) {
-		launch();
-	}
-
+	static BorderPane border=new BorderPane();
+	static TilePane left=new TilePane();	//BorderPane's left component
+	
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void start(Stage mainStage) throws Exception {
+		Scene scene=new Scene(new Label("Welcome to Minesweeper!"), 400,200);
+		
+		border.getChildren().add(left);
+		
+		mainStage.setTitle("Minesweeper");
+		mainStage.setScene(scene);
+		mainStage.show();
+		
 	}
-
+	
+	public static void main(String args[]) {
+		Application.launch();
+	}
+	
+	
 }
