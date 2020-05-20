131cd71fe02d76cb86893573be685f5cd207d47a
likitha9 <sivalikitha.valluru9@gmail.com>
2020-05-19 20:55:40 -0400

    Adding random mines

--- Minesweeper/src/MineField.java
+++ Minesweeper/src/MineField.java
@@ -3,9 +3,11 @@ import javafx.scene.layout.Pane;
 public class MineField {
 
 	public static Pane generateBoard(int size, int mines) {
-		return EmptyField.createBoard(size, mines);
+
+		Pane pane = EmptyField.createBoard(size, mines); // create empty board
+		pane = MinesToEmptyField.addMines(pane, mines); // add mines to empty board
+
+		return pane;
 	}
 
 }
-
-
---
+++ Minesweeper/src/MinesToEmptyField.java
@@ -0,0 +1,20 @@
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author likit
+ *
+ *         Given an empty mine field, add mines randomly. Extract this number
+ *         from either preset data or custom data.
+ *
+ */
+public class MinesToEmptyField extends EmptyField {
+
+	// static int layout[][]=new int[][]
+	public static Pane addMines(Pane pane, int mines) {
+
+		return pane;
+
+	}
+
+}
