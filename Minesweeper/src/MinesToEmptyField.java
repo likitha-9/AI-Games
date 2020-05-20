949eaa6269d653c5be61ee25c72e21e91253baa6
likitha9 <sivalikitha.valluru9@gmail.com>
2020-05-19 22:25:36 -0400

    Update MinesToEmptyField.java
    Mines added

--- Minesweeper/src/MinesToEmptyField.java
+++ Minesweeper/src/MinesToEmptyField.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.Shadow;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 /**
  *
@@ -19,24 +23,23 @@ public class MinesToEmptyField extends EmptyField {
 		// The board reads in grid[column_number][row_number] format.
 		int columns = EmptyField.grid.length, rows = EmptyField.grid[0].length;
 
-		//Keep track of where mines are being added.
-		ArrayList<ArrayList<Integer>> coords=new ArrayList<>();
-		for(int i=0;i<columns;i++)
+		// Keep track of where mines are being added.
+		ArrayList<ArrayList<Integer>> coords = new ArrayList<>();
+		for (int i = 0; i < columns; i++)
 			coords.add(new ArrayList<Integer>());
 
-		int placedMines = 0; //initially zero
+		int placedMines = 0; // initially zero
 
-		Random rand = new Random();	//to place mines randomly
+		Random rand = new Random(); // to place mines randomly
 
 		while (placedMines < mines) {
-			int randomCol = rand.nextInt(columns), randomRow = rand.nextInt(rows);
-
-			if(coords.get(randomCol).contains(randomRow))
-				continue;
-			else
-			{
-				coords.get(randomCol).add(randomRow);
-				placedMines+=1;
+			int randomCol = rand.nextInt(columns), randomRow = rand.nextInt(rows);// generate random column/row numbers
+			if (coords.get(randomCol).contains(randomRow)) // check if mine is already present: location [col][row]
+				continue; // if present, continue; placedMines+=0
+			else {
+				coords.get(randomCol).add(randomRow); // if mine isn't present, add location into list
+				placedMines += 1;
+				EmptyField.grid[randomCol][randomRow].border.setEffect(changeShadow(EmptyField.grid[randomCol][randomRow].border));
 			}
 		}
 		for (int i = 0; i < columns; i++) {
@@ -46,6 +49,15 @@ public class MinesToEmptyField extends EmptyField {
 		}
 		return pane;
 
+	}
+
+	static Shadow changeShadow(Rectangle border) {
+		Shadow shadow = new Shadow();
+		shadow.setBlurType(BlurType.GAUSSIAN);
+		shadow.setColor(Color.RED);
+		shadow.setRadius(4);
+		border.setEffect(shadow);
+		return shadow;
 	}
 
 }
