91668a3f657e966dc807073a20852fb953d41f77
likitha9 <sivalikitha.valluru9@gmail.com>
2020-05-19 22:14:41 -0400

    Update MinesToEmptyField.java

--- Minesweeper/src/MinesToEmptyField.java
+++ Minesweeper/src/MinesToEmptyField.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 import javafx.scene.layout.Pane;
 
 /**
@@ -13,6 +16,34 @@ public class MinesToEmptyField extends EmptyField {
 	// static int layout[][]=new int[][]
 	public static Pane addMines(Pane pane, int mines) {
 
+		// The board reads in grid[column_number][row_number] format.
+		int columns = EmptyField.grid.length, rows = EmptyField.grid[0].length;
+
+		//Keep track of where mines are being added.
+		ArrayList<ArrayList<Integer>> coords=new ArrayList<>();
+		for(int i=0;i<columns;i++)
+			coords.add(new ArrayList<Integer>());
+
+		int placedMines = 0; //initially zero
+
+		Random rand = new Random();	//to place mines randomly
+
+		while (placedMines < mines) {
+			int randomCol = rand.nextInt(columns), randomRow = rand.nextInt(rows);
+
+			if(coords.get(randomCol).contains(randomRow))
+				continue;
+			else
+			{
+				coords.get(randomCol).add(randomRow);
+				placedMines+=1;
+			}
+		}
+		for (int i = 0; i < columns; i++) {
+			for (int j = 0; j < rows; j++) {
+
+			}
+		}
 		return pane;
 
 	}
